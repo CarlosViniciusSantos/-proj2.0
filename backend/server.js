1a305bfd913340e55ef727129ec161b723f7e763
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-05-20 16:37:40 -0300

    começo backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Olá, Servidor HTTP!');
+});
+server.listen(3000, '127.0.0.1', () => {
+console.log('Servidor rodando em http://127.0.0.1:3000/');
+});
