4901954074909b7472f8b2558f5ce61af06f7374
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-05-23 00:23:05 -0300

    crud usuarios

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,24 +1,57 @@
 const express = require("express")
 const router = express.Router()
+const {PrismaClient} = require("@prisma/client")
+const prisma = new PrismaClient()
 
 // get post put delete
 
-router.get("/1",(req,res)=>{
-    res.json({menssagem:"METODO: GET"})
-});
+router.get("/",async (req,res)=>{
 
-router.post("/:id",(req,res)=>{
-    const body = req.body
+    const users = await prisma.usuario.findMany()
+    res.json({menssagem:"usuarios listados com sucesso", users})
+});
+router.get("/:id",async (req,res)=>{
     const {id} = req.params
-    res.json({body, id})
+    const user = await prisma.usuario.findUnique({ 
+        where:{
+            id:+id
+
+        }
+    })
+    res.json({menssagem:"usuarios listados com sucesso", user})
 });
 
-router.put("/",(req,res)=>{
-    res.json({menssagem:"METODO: PUT"})
+router.post("/",async (req,res)=>{
+    const body = req.body
+    
+    const user = await prisma.usuario.create({
+        data: body
+    })
+    res.json({menssagem: "usuario criado com sucesso", user})
 });
 
-router.delete("/",(req,res)=>{
-    res.json({menssagem:"METODO: DELETE"})
+router.put("/:id", async(req,res)=>{
+    const {id} = req.params
+    const data = req.body
+    const user = await prisma.usuario.update({ 
+        where:{
+            id:+id
+
+        },
+        data
+    })
+    res.json({menssagem:"usuarios atualizado com sucesso", user})
+});
+
+router.delete("/:id",async(req,res)=>{
+    const {id} = req.params
+    const user = await prisma.usuario.delete({ 
+        where:{
+            id:+id
+
+        }
+    })
+    res.json({menssagem:"usuario deletado com sucesso", user})
 });
 
 module.exports = router
